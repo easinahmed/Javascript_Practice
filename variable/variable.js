714221e4a37aa067a7ab6db51c35e554da7e299a
easinahmed <easintshirtdesigner@gmail.com>
2025-10-05 21:07:12 +0600

    Practice Ongoing

---
+++ variable/variable.js
@@ -0,0 +1,96 @@
+const birthYear = 2005;
+const siblings = 2;
+const students = 160;
+const marks = 92;
+const rooms = 3;
+const readingHours = 2;
+const total = 0+1+8+1+1+5+1+5+5+8+8;
+
+console.log(total);
+
+
+// Practice -2 
+const age = 21;
+console.log(age);
+
+const booksToRead = 10;
+console.log(booksToRead);
+
+const wakeUpTime = 5;
+console.log(wakeUpTime);
+
+const borrow = 6200;
+console.log(borrow);
+
+const goals = 3;
+console.log(goals);
+
+const pillow = 1;
+console.log(pillow);
+
+
+// Practice -3 String
+
+const name = "Easin Ahmed"
+console.log(name);
+
+const address = "Mirpur 10"
+console.log(address);
+
+const college = "Bhawal Badre Alam Govt College"
+console.log(college);
+
+const fan = "Durrefishan, Dananeer"
+console.log(fan);
+
+const movieOfTheYear = "Housefull-5"
+console.log(movieOfTheYear);
+
+const hobby = "Programming"
+console.log(hobby);
+
+const color = "Black"
+console.log(color);
+
+
+
+// Practice -4 Boolean
+const isHappy = false;
+console.log(isHappy);
+
+const isCrushReply = true;
+console.log(isCrushReply);
+
+const celebrating = true;
+console.log(celebrating);
+
+const hasMoney = false;
+const isHungry = true;
+console.log(hasMoney, isHungry);
+
+
+const isStudyDone = false;
+console.log(isStudyDone);
+
+
+const isPhoneCharged = false;
+console.log(isPhoneCharged);
+
+const isDinnerDone = false;
+console.log(isDinnerDone);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
