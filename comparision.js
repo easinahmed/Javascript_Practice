d5160baf6780b66c184da85e0bb872460d6b586f
easinahmed <easintshirtdesigner@gmail.com>
2025-10-10 23:00:20 +0600

    Condition practice

---
+++ comparision.js
@@ -0,0 +1,23 @@
+// comparision practice 
+// comparision operators
+// { > , < , >=, <=, ==, ===, != , !== }
+
+const peyaraPrice = 200
+const applePrice = 400 
+console.log(peyaraPrice<=applePrice);
+
+const myName = "Easin"
+const friendName = "Polash"
+
+console.log(myName=== friendName);
+
+
+
+console.log( "test" === "TEST");
+
+
+const A = 'Hello';
+const B = "Hello"
+console.log(A === B);
+
+
---
+++ condition.js
@@ -0,0 +1,61 @@
+const biriyaniPrice = 400;
+if (biriyaniPrice < 300) {
+    console.log("Mama biriyani dew");
+    
+} else {
+    console.log("chol puri khai");
+    
+}
+
+
+const rainingOutside = true;
+if (rainingOutside) {
+  console.log("You and me - under the tree");
+      
+}else{
+    console.log("no rain. No romance");
+    
+}
+
+
+const salary = 63000
+const isBCS = true
+
+if (salary > 50000 && isBCS) {
+    console.log("Mama sei lvl er ---- patro");
+    
+    
+}else{
+    console.log("dure jaiya mor hala");
+    
+}
+
+const isSalary = 23000
+const height  = 64
+
+if (salary > 50000 || height > 72) {
+    console.log("bolo baba kobul");
+    
+    
+}else{
+    console.log("vaag tui mokbul");
+    
+}
+
+const price = 4000
+if (price>= 5000) {
+    const discount = (price/100)*15    
+    const pay = price - discount
+    console.log(pay);
+    
+}else if (price >=2500 ){
+    const discount = (price/100)*5    
+    const pay = price - discount
+    console.log(pay);
+    
+    
+}else{
+    console.log(price);
+    
+}
+
