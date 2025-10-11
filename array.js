872bdfb4999df8e319323cfc9247a9a21a6ae640
easinahmed <easintshirtdesigner@gmail.com>
2025-10-11 20:02:51 +0600

    Basic array practice

---
+++ array.js
@@ -0,0 +1,30 @@
+const arr = [1,4,8,10,45,67]
+console.log(arr);
+
+const len = arr.length
+console.log(len);
+
+const indexPosition = arr[2]
+console.log(indexPosition);
+
+arr[2]= 6
+
+
+// arr.push(78)
+arr.pop()
+// arr.shift()
+arr.unshift(0)
+console.log(arr);
+
+const friends = ["polash", "munib", "amir", "alamin"]
+if (friends.includes("alamin")) {
+    console.log("lo buffet jai");
+    
+}else{
+    console.log("teha nai");
+    
+}
+
+const index = friends.indexOf("alamin")
+console.log(index);
+
